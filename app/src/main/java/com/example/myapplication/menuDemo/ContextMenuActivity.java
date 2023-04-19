3a81a6e3d911a05ac2cdc82826455a61ba3fe657
zxl <2483745934@qq.com>
2023-04-19 14:47:01 +0800

    popup menu

--- app/src/main/java/com/example/myapplication/menuDemo/ContextMenuActivity.java
+++ app/src/main/java/com/example/myapplication/menuDemo/ContextMenuActivity.java
@@ -1,10 +1,12 @@
 package com.example.myapplication.menuDemo;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import com.example.myapplication.R;
 
 public class ContextMenuActivity extends AppCompatActivity {
     private Button button;
+    private LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,7 @@ public class ContextMenuActivity extends AppCompatActivity {
         button = findViewById(R.id.context_menu);
         registerForContextMenu(button);
         button.setOnClickListener((View::showContextMenu));
+        linearLayout = findViewById(R.id.context_menu_layout);
     }
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
@@ -31,9 +35,11 @@ public class ContextMenuActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.red:{
                 Toast.makeText(this, "你选择红色", Toast.LENGTH_SHORT).show();
+                linearLayout.setBackgroundColor(Color.RED);
             };break;
             case R.id.blue:{
                 Toast.makeText(this,"你选择了蓝色",Toast.LENGTH_SHORT).show();
+                linearLayout.setBackgroundColor(Color.BLUE);
             };break;
             default:{
                 Toast.makeText(this,"没有选择",Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/myapplication/menuDemo/PopupActivity.java
+++ app/src/main/java/com/example/myapplication/menuDemo/PopupActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication.menuDemo;
 
 
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.myapplication.R;
 
@@ -13,6 +16,15 @@ public class PopupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popup);
         button = findViewById(R.id.popup_menu);
+        PopupMenu popupMenu = new PopupMenu(this, button);
+        MenuInflater menuInflater = this.getMenuInflater();
+        menuInflater.inflate(R.menu.popup_menu,popupMenu.getMenu());
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupMenu.show();
+            }
+        });
     }
 
 }
